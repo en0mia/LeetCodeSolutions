d48a84f6a426006df7a06f12b9244d2debf0c34d
Simone Nicol <en0mia.dev@gmail.com>
2022-10-23 18:57:18 +0200

    :sparkles: Add RemoveDuplicatesFromSortedList.

---
+++ src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,49 @@
+/*
+    @author: Simone Nicol <en0mia.dev@gmail.com>
+    @created: 23/10/22
+    @copyright: Check the repository license.
+*/
+
+public class RemoveDuplicatesFromSortedList {
+    // Implementation provided by the problem
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode previous = head;
+        ListNode current = previous.next;
+
+        while (current != null) {
+            if (current.val == previous.val) {
+                // it is a duplicate: drop it
+                previous.next = current.next;
+            } else {
+                // Previous should proceed only if we drop an element
+                previous = current;
+            }
+
+            // Current should always proceed
+            current = current.next;
+        }
+
+        return head;
+    }
+}
