d8312f19fc4174014dc2d664b8b7126ebb0afab6
Simone Nicol <en0mia.dev@gmail.com>
2022-10-27 00:07:24 +0200

    :art: Improve the solution using LinkedHashSet instead of a DoubleLinkedList.

--- src/LFUCache.java
+++ src/LFUCache.java
@@ -5,15 +5,16 @@
 */
 
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.TreeMap;
 
 // Problem link: https://leetcode.com/problems/lfu-cache/
 public class LFUCache {
     private final int capacity;
-    private final TreeMap<Integer, DoublyLinkedList> frequencies = new TreeMap<>();
+    private final TreeMap<Integer, LinkedHashSet<Integer>> frequencies = new TreeMap<>();
     private final Map<Integer, Integer> keyFrequency = new HashMap<>();
-    private final Map<Integer, Node> values = new HashMap<>();
+    private final Map<Integer, Integer> values = new HashMap<>();
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
@@ -24,12 +25,11 @@ public class LFUCache {
             return -1;
         }
 
-        final Node node = values.get(key);
         final int frequency = keyFrequency.get(key);
 
         updateFrequency(key, frequency, frequency + 1);
 
-        return node.val;
+        return values.get(key);
     }
 
     public void put(int key, int value) {
@@ -38,23 +38,17 @@ public class LFUCache {
         }
 
         if (!values.containsKey(key)) {
-            final Node node = new Node(key, value);
-
             if (values.size() == capacity) {
                 resize();
             }
 
             // Insert the new node
-            frequencies.computeIfAbsent(1, k -> new DoublyLinkedList());
-            frequencies.get(1).insert(node);
-            values.put(key, node);
+            frequencies.computeIfAbsent(1, k -> new LinkedHashSet<>());
+            frequencies.get(1).add(key);
+            values.put(key, value);
             keyFrequency.put(key, 1);
         } else {
-            final Node node = values.get(key);
-
-            // Update the value
-            node.val = value;
-
+            values.put(key, value);
             final int frequency = keyFrequency.get(key);
             updateFrequency(key, frequency, frequency + 1);
         }
@@ -63,11 +57,11 @@ public class LFUCache {
     private void resize() {
         // Remove least used element with least frequency
         final int lowestFreq = frequencies.firstKey();
-        final DoublyLinkedList list = frequencies.get(lowestFreq);
-        final Node leastUsed = list.getFirst();
+        final LinkedHashSet<Integer> list = frequencies.get(lowestFreq);
+        final int leastUsed = list.iterator().next();
         list.remove(leastUsed);
-        values.remove(leastUsed.key);
-        keyFrequency.remove(leastUsed.key);
+        values.remove(leastUsed);
+        keyFrequency.remove(leastUsed);
 
         if (frequencies.get(lowestFreq).isEmpty()) {
             frequencies.remove(lowestFreq);
@@ -75,73 +69,21 @@ public class LFUCache {
     }
 
     private void updateFrequency(final int key, final int oldFrequency, final int newFrequency) {
-        final Node node = values.get(key);
-        final DoublyLinkedList valuesList = frequencies.get(oldFrequency);
+        final LinkedHashSet<Integer> valuesList = frequencies.get(oldFrequency);
+        final int value = values.get(key);
 
         // Remove the node from the old frequency
-        valuesList.remove(node);
+        valuesList.remove(key);
 
         if (frequencies.get(oldFrequency).isEmpty()) {
             frequencies.remove(oldFrequency);
         }
 
-        frequencies.computeIfAbsent(newFrequency, k -> new DoublyLinkedList());
+        frequencies.computeIfAbsent(newFrequency, k -> new LinkedHashSet<>());
 
         // Insert the node
-        final Node newNode = new Node(node.key, node.val);
-        frequencies.get(newFrequency).insert(newNode);
+        frequencies.get(newFrequency).add(key);
         keyFrequency.put(key, newFrequency);
-        values.put(key, newNode);
-    }
-
-    private static class Node {
-        public int key, val;
-        public Node previous, next;
-        public Node(int key, int val) {
-            this.key = key;
-            this.val = val;
-        }
-    }
-
-    // Auxiliary classes.
-    private static class DoublyLinkedList {
-        public Node head, tail;
-        public int n = 0;
-        public Node getFirst() {
-            return head;
-        }
-
-        // Insert at the end
-        public void insert(final Node node) {
-            if (head == null) {
-                head = node;
-            } else {
-                tail.next = node;
-                node.previous = tail;
-            }
-
-            tail = node;
-            n ++;
-        }
-
-        public void remove (final Node node) {
-            if (node.next == null) {
-                tail = node.previous;
-            } else {
-                node.next.previous = node.previous;
-            }
-
-            if (node.previous == null) {
-                head = node.next;
-            } else {
-                node.previous.next = node.next;
-            }
-
-            n --;
-        }
-
-        public boolean isEmpty() {
-            return n == 0;
-        }
+        values.put(key, value);
     }
 }
